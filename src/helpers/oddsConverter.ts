7a5be3d6469578378f25e2bdd99d33eb3a888418
Emilio Font <emifont01@gmail.com>
2020-08-31 13:24:09 -0400

    AOEB-9 AOEB-10 addes jest and simple test example for odds

--- src/helpers/oddsConverter.ts
+++ src/helpers/oddsConverter.ts
@@ -2,16 +2,13 @@ import { Odd } from "../model/odd";
 
 
 export class OddsConverter {
+    
     constructor() {
         
     }
    
    public async fromProbability(prob: number): Promise<Odd>{
-    // Decimal - 1 divided by (the percentage divided by 100) e.g. a probability of 50% = 1 / (50 / 100) = 2.
-    // Fraction - (1 divided by (the percentage divided by 100)) minus 1 e.g. a probability of 25% = (1 / (25 / 100)) - 1 = 3 = 3/1.
-    // American:
-    // Positive odds - (100 divided by (the percentage divided by 100)) minus 100 e.g. a probability of 10% = (100 / (10 / 100)) - 100 = 900.
-    // Negative odds - The probability divided by (1 minus (the probability divided by 100)) then multiply by -1 to convert into a negative e.g. a probability of 20% = (20 / (1 - (20/100))) * -1 = -25.
+
     const decimal = 1 / (prob / 100);
     let american = 0;
     if(prob <= 50){
@@ -19,15 +16,42 @@ export class OddsConverter {
     }else{
         american = prob / ( 1- (prob/100)) * -1;
     }
-
+    
     return new Odd(decimal, 0, american, prob);
    }
 
-   public async fromDecimal(decimalOdd: number){
-    // Positive american odds - (The decimal value minus one) multiplied by 100 e.g. a decimal value of 3 = (3 - 1) * 100 = 200.
-    // Negative american odds - Negative 100 divided by (the decimal odds minus one) e.g. a decimal value of 1.2 = -100 / (1.2 - 1) = -500.
-     const positiveAmerican = (decimalOdd - 1) * 100;
-     const negativeAmerican = -100 / (decimalOdd - 1);
+   public async fromDecimal(decimalOdd: number): Promise<Odd>{
+   
+    let american = 0;
+    
+    if(decimalOdd >=2){
+        american = (decimalOdd - 1) * 100;
+    }else{
+        american = -100 / (decimalOdd - 1);
+    }
+    //TODO: missing fractional.
+
+    const impliedProbability =   (1 / decimalOdd) * 100 ;
+    
+    return new Odd(decimalOdd, 0, american, impliedProbability);
+   }
+
+   public async fromAmerican(americanOdd: number): Promise<Odd>{
+   
+    let decimal = 0;
+    let impliedProbability = 0;
+    //TODO: missing fractional.
+
+    if(americanOdd > 0){
+        impliedProbability = (100 / (americanOdd + 100)) * 100;
+        decimal =  1 + (americanOdd / 100);
+    }else{
+        americanOdd*= -1;
+        impliedProbability =  (americanOdd/(americanOdd+100)) * 100;
+        decimal = 1 - (100/americanOdd)
+    }
+
+    return new Odd(decimal,0,americanOdd, impliedProbability);
    }
 
 }
